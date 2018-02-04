248c9ca588a2e5629b432873f1cfe8a5504c2fba
Vladimir <vyrnos@github.com>
2018-02-04 11:20:21 +0100

    First commit

---
+++ app/src/main/java/com/g81vdbvf/usermanager/PantallaPresentacion.java
@@ -0,0 +1,13 @@
+package com.g81vdbvf.usermanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaPresentacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_presentacion);
+    }
+}
