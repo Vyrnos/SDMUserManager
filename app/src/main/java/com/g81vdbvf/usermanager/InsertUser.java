b421c08dadd6daa18d1ed41273ee87ebbe78a913
Vladimir <vyrnos@github.com>
2018-02-05 22:31:50 +0100

    Permiso de ver el estado de la red en el manifest (creo que innecesario) Cambio del archivo InsertUser.java para hacer la query correctamente a la pagina web (excepto el filtro de registered), se añaden a una lista de contactos provisional, ya se verá en un futuro como guardar esto en sqlite

--- app/src/main/java/com/g81vdbvf/usermanager/InsertUser.java
+++ app/src/main/java/com/g81vdbvf/usermanager/InsertUser.java
@@ -1,5 +1,6 @@
 package com.g81vdbvf.usermanager;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,15 +9,22 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 public class InsertUser extends AppCompatActivity {
 
     Button insertCallAPI;
-    EditText nacionality, numUsers, regDate;
+    EditText nationality, numUsers, regDate;
     CheckBox male, female;
 
     @Override
@@ -24,45 +32,94 @@ public class InsertUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_user);
 
-        nacionality = findViewById(R.id.nacionality);
-        numUsers = findViewById(R.id.numUsers);
-        regDate = findViewById(R.id.regDate);
+        nationality = (EditText) findViewById(R.id.nacionality);
+        numUsers = (EditText) findViewById(R.id.numUsers);
+        //regDate = findViewById(R.id.regDate);
 
-        male = findViewById(R.id.checkMale);
-        female = findViewById(R.id.checkFemale);
-
-        StringBuilder sb = new StringBuilder("https://randomuser.me/api/?inc=gender,registered,nat");
-        if(nacionality.getText().length()>0) sb.append("&nat="+nacionality.getText());
-        if(numUsers.getText().length()>0) sb.append("&results="+nacionality.getText());
-        if(regDate.getText().length()>0) sb.append("&registered="+nacionality.getText());
+        male = (CheckBox) findViewById(R.id.checkMale);
+        female = (CheckBox) findViewById(R.id.checkFemale);
 
         insertCallAPI = (Button) findViewById(R.id.insertCallAPI);
         insertCallAPI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                StringBuilder sb = new StringBuilder("https://randomuser.me/api/?inc=gender,registered,nat");
+
+                if(nationality.getText().length()>0) sb.append("&nat="+nationality.getText());
+                if(numUsers.getText().length()>0) sb.append("&results="+numUsers.getText());
+
+                if(male.isChecked()){
+                    sb.append("&gender=male");
+                    if(female.isChecked()) sb.append(",female");
+                }else if(female.isChecked()) sb.append("&gender=female");
+
+                String request = null;
                 try {
-                    URL url = new URL("https://randomuser.me/api/");
-
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    try {
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                        StringBuilder stringBuilder = new StringBuilder();
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {
-                            stringBuilder.append(line).append("\n");
-                        }
-                        bufferedReader.close();
-
-                    }
-                    finally{
-                        urlConnection.disconnect();
-                    }
+                    request = new GetUrlContentTask().execute(sb.toString()).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
                 }
-                catch(Exception e) {
-                    Log.e("ERROR", e.getMessage(), e);
+
+                Log.v("INSERT_USER", "REQUEST TO STRING IS "+ request);
+                try {
+                    JSONObject reader = new JSONObject(request);
+                    JSONArray results = reader.getJSONArray("results");
+                    ArrayList<HashMap<String,String>> contactList = new ArrayList<>();
+                    for(int i = 0; i < results.length(); i++) {
+                        JSONObject obj = results.getJSONObject(i);
+                        String gender = obj.getString("gender");
+                        String registered = obj.getString("registered");
+                        HashMap<String, String> contact = new HashMap<>();
+                        contact.put("gender",gender);
+                        contact.put("registered",registered);
+                        contactList.add(contact);
+                    }
+                    for(int i = 0; i < contactList.size(); i++){
+                        Log.v("CONTACTOSSSS","El contacto es: GENERO: " + contactList.get(i).get("gender") + " REGISTRADO EN: "+ contactList.get(i).get("registered"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
+
             }
         });
 
+    }
+}
+
+class GetUrlContentTask extends AsyncTask<String, Integer, String> {
+    String content = "";
+
+    public GetUrlContentTask(){
+
+    }
+    protected String doInBackground(String... urls) {
+
+        try{
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            try{
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.setDoOutput(false);
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+                connection.connect();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    content += line + "\n";
+                }
+
+            }finally {
+                connection.disconnect();
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content;
     }
 }
