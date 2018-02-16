50f7dc322cbea0ea0001537401b7d7af07047ec6
Vladimir <vyrnos@github.com>
2018-02-16 18:23:16 +0100

    Me gustaría quitar todos los warnings pero en principio ya esta la app al completo, se puede ir probando en busca de fallos y hacer la memoria

--- app/src/main/java/com/g81vdbvf/usermanager/PantallaPrincipal.java
+++ app/src/main/java/com/g81vdbvf/usermanager/PantallaPrincipal.java
@@ -2,20 +2,19 @@ package com.g81vdbvf.usermanager;
 
 import android.arch.persistence.room.Room;
 import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class PantallaPrincipal extends AppCompatActivity {
-
+public class PantallaPrincipal extends BaseActivity {
     Button list, insert, borrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_principal);
 
         list=(Button) findViewById(R.id.buttonList);
         insert=(Button) findViewById(R.id.buttonInsert);
@@ -44,5 +43,16 @@ public class PantallaPrincipal extends AppCompatActivity {
                 Toast.makeText(PantallaPrincipal.this ,"Usuarios borrados con éxito",Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_pantalla_principal;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_home;
     }
 }
